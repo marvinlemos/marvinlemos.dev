1c7bcfebe47a3c1014373ce04ad3dedf4caaad78
Marvin Lemos <marvinlemos@gmail.com>
2021-01-02 16:39:43 -0300

    First commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import '../css/style.css'
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
---
+++ utils/getUser.js
@@ -0,0 +1,31 @@
+const getUser = async(username) => {
+    const resUser = await fetch(`https://api.github.com/users/${username}`)
+    const user = await resUser.json()
+
+    const resRepos = await fetch(`https://api.github.com/users/${username}/repos?sort=updated`)
+    const originalRepos = await resRepos.json()
+
+    const dontShowRepos = ['marvinlemos/idswsn', 'marvinlemos/kaggle-google-revenue']
+
+    const dontShowFilter = repo => dontShowRepos.indexOf(repo.full_name) === -1
+    const isNotForkFilter = repo => !repo.fork
+
+    const extractData = repo => ({
+        id: repo.id,
+        full_name: repo.full_name,
+        language: repo.language,
+        stargazers_count: repo.stargazers_count
+    })
+
+    const repos = originalRepos
+        .filter(isNotForkFilter)
+        .filter(dontShowFilter)
+        .map(extractData)
+    
+    return {
+        repos,
+        user
+    }
+}
+
+export default getUser
