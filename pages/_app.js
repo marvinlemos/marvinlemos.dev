69fd41922c983e890f0c8443304a45be6441d286
Marvin Lemos <marvinlemos@gmail.com>
2021-01-02 18:52:20 -0300

    Added Font titilium web

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
+import App from 'next/app'
 
 import '../css/style.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MyApp extends App {
+    render() {
+        const { Component, pageProps } = this.props
+        return (
+            <React.Fragment>
+                <style jsx global>
+                    {`
+                    @import url('https://fonts.googleapis.com/css2?family=Titillium+Web:wght@400;700&display=swap');
+                    `}
+                </style>
+                <Component {...pageProps} />
+            </React.Fragment>
+        )
+    }
 }
 
 export default MyApp
